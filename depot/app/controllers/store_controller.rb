8b730fe0ffd43989e41bf18e10e756e863b9856c
Daniel Meneguin Barbosa <meneguin.daniel@gmail.com>
2021-11-07 18:42:01 -0300

    Task D

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -2,6 +2,8 @@
 
 class StoreController < ApplicationController
   def index
+    session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1
+    @accesses = session[:counter]
     @products = Product.order(:title)
   end
 end
