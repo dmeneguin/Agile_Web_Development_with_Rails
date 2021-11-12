13ba3107fbb84dd1d281b27a39cadbafae91aa91
Daniel Meneguin Barbosa <meneguin.daniel@gmail.com>
2021-11-11 22:39:18 -0300

    finished task J

--- depot/config/environment.rb
+++ depot/config/environment.rb
@@ -5,3 +5,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :test
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    user_name: ENV["EMAIL_ADDRESS"],
+    password: ENV["EMAIL_PASSWORD"],
+    enable_starttls_auto: true
+    }
+end
